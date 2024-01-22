072eb499957162def7751520cc7e367479c16184
Punkbooster9000 <arsen@loopmasters.com>
2024-01-22 13:01:28 +0100

    Use ollama embedings, quadrant and remove unnecessary dependencies

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-import os
-
-from langchain.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Pinecone
+from langchain_community.vectorstores import Qdrant
 from langchain.chains import RetrievalQA
-import pinecone
-
-from langchain.llms import Ollama
-
-pinecone.init(api_key=os.environ.get("PINECONE_API_KEY"), environment="gcp-starter")
+from langchain_community.llms import Ollama
+from langchain_community.embeddings import OllamaEmbeddings
 
 if __name__ == "__main__":
-  print("hello Vector")
-
   # initialize the loader
   loader = TextLoader(
     "/Users/punkbooster/Projects/langchain/midium-blogs/medium-vector-database.txt"
@@ -27,29 +19,25 @@ if __name__ == "__main__":
 
   # split the text
   texts = text_splitter.split_documents(document)
-  # command to check for how many chunks the text is splitted
-  # print(len(texts))
 
   # initialize Embeddings. Embeding model accepts text and transforms it as a vector.
-  embeddings = OpenAIEmbeddings(
-    openai_api_key=os.environ.get("OPENAI_API_KEY")
-  )
+  embeddings = OllamaEmbeddings()
 
-  # few things we are doing here:
-  # take the text chunks, use openai embeddings api to convert text chunks to vectors, take these vectors and store them in pinecone database
-  # index name is database name index created from pinecone.io
-  doc_search = Pinecone.from_documents(
-    texts, embeddings, index_name="langchain-doc-index"
+  qdrant = Qdrant.from_documents(
+    texts,
+    embeddings,
+    location=":memory:",
+    collection_name="my_documents",
   )
 
   # connect to local ollama
   llm = Ollama(base_url="http://localhost:11434", model="llama2")
 
-  # create a chain with OpenAI LLm
+  # create a chain with Ollama embedded LLm
   # chain type stuff means that the context for LLm we are going to provide by ourselves from vector store
   # retriever is being used from a vector store
   qa = RetrievalQA.from_chain_type(
-    llm=llm, chain_type="stuff", retriever=doc_search.as_retriever()
+    llm=llm, chain_type="stuff", retriever=qdrant.as_retriever()
   )
 
   query = "What is a Vector DB? Give me a 15 word answer."
